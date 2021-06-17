823e05d39489552668dc5b79e2e1ba67617262d9
Prasad Viduranga <pkpviduranga@gmail.com>
2021-06-18 00:27:12 +0530

    Created appInitializer and main form

---
+++ src/controller/MainFormController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+
+public class MainFormController {
+    public JFXButton btnDashboard;
+    public JFXButton btnManageUsers;
+    public JFXButton btnAddNewPayment;
+    public JFXButton btnReports;
+
+    public void btnReports_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddNewPayment_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnManageUsers_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDashboard_OnAction(ActionEvent actionEvent) {
+    }
+}
