038052893d21eb10cd193db0284f96e4103cde02
Prasad Viduranga <pkpviduranga@gmai.com>
2021-07-11 00:11:46 +0530

    Move some of codes to HomeFormController from MainFormController

--- src/controller/HomeFormController.java
+++ src/controller/HomeFormController.java
@@ -1,4 +1,81 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HomeFormController {
+
+    public JFXButton btnDashboard;
+    public JFXButton btnManageUsers;
+    public JFXButton btnAddNewPayment;
+    public JFXButton btnReports;
+    public AnchorPane contextOfHomeForm;
+
+
+    public void btnReports_OnAction(ActionEvent actionEvent) throws IOException {
+        String url = "../views/ReportsForm.fxml";
+        navigate(url, "Reports");
+    }
+
+    public void btnAddNewPayment_OnAction(ActionEvent actionEvent) throws IOException {
+        String url = "../views/AddNewPaymentForm.fxml";
+        navigate(url, "Add New Payment");
+    }
+
+    public void btnManageUsers_OnAction(ActionEvent actionEvent) throws IOException {
+        String url = "../views/ManageUserForm.fxml";
+        navigate(url, "Manage User");
+    }
+
+    public void btnDashboard_OnAction(ActionEvent actionEvent) throws IOException {
+        String url = "../views/DashboardForm.fxml";
+        navigate(url, "DashBoard");
+    }
+
+    public void btnManageUser_OnKeyPress(KeyEvent keyEvent) throws IOException {
+        if ((keyEvent.getCode() == KeyCode.getKeyCode("Enter")) || (keyEvent.getCode() == KeyCode.getKeyCode("Space"))) {
+            String url = "../views/ManageUserForm.fxml";
+            navigate(url, "Manage Users");
+        }
+    }
+
+    public void btnAddNewPayment_OnKeyPress(KeyEvent keyEvent) throws IOException {
+        if ((keyEvent.getCode() == KeyCode.getKeyCode("Enter")) || (keyEvent.getCode() == KeyCode.getKeyCode("Space"))) {
+            String url = "../views/AddNewPaymentForm.fxml";
+            navigate(url, "Add New Payment");
+        }
+    }
+
+    public void btnReports_OnKeyPress(KeyEvent keyEvent) throws IOException {
+        if ((keyEvent.getCode() == KeyCode.getKeyCode("Enter")) || (keyEvent.getCode() == KeyCode.getKeyCode("Space"))) {
+            String url = "../views/ReportsForm.fxml";
+            navigate(url, "Reports");
+        }
+    }
+
+    public void btnDashboard_OnKeyPress(KeyEvent keyEvent) throws IOException {
+        if ((keyEvent.getCode() == KeyCode.getKeyCode("Enter")) || (keyEvent.getCode() == KeyCode.getKeyCode("Space"))) {
+            String url = "../views/DashboardForm.fxml";
+            navigate(url, "Dashboard");
+        }
+    }
+
+    public void navigate(String url, String title) throws IOException {
+        Stage stage = (Stage) contextOfHomeForm.getScene().getWindow();
+        Parent root = FXMLLoader.load(this.getClass().getResource(url));
+        Scene dashboard = new Scene(root);
+        stage.setScene(dashboard);
+        //lblTitle.setText(title);
+        stage.setResizable(false);
+        stage.show();
+    }
 }
--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,25 +1,14 @@
 package controller;
 
-import com.jfoenix.controls.JFXButton;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
-import java.io.IOException;
 
 public class MainFormController {
-    public JFXButton btnDashboard;
-    public JFXButton btnManageUsers;
-    public JFXButton btnAddNewPayment;
-    public JFXButton btnReports;
+
     public AnchorPane contextOfMainForm;
     public ImageView imgClose;
     public Label lblTitle;
@@ -29,20 +18,20 @@ public class MainFormController {
     public ImageView img_Minimize;
     public ImageView imgMinimize;
     public AnchorPane pneAppBar;
-    private double xpos;
-    private double ypos;
+    private double xPos;
+    private double yPos;
 
     public void initialize() {
-        lblTitle.setText("IJSE Student Management System.");
+        lblTitle.setText("IJSE Student Management System");
         pneAppBar.setOnMousePressed(event -> {
-            xpos=event.getX();
-            ypos=event.getY();
+            xPos = event.getX();
+            yPos = event.getY();
         });
 
         pneAppBar.setOnMouseDragged(event -> {
             Window mainWindow = imgClose.getScene().getWindow();
-            mainWindow.setX(event.getScreenX()-xpos);
-            mainWindow.setY(event.getScreenY()-ypos);
+            mainWindow.setX(event.getScreenX() - xPos);
+            mainWindow.setY(event.getScreenY() - yPos);
         });
 
         imgClose.setOnMouseEntered(event -> imgClose.setImage(new Image("views/asserts/icons/hovers/close_hover.png")));
@@ -62,61 +51,5 @@ public class MainFormController {
 
     }
 
-    public void btnReports_OnAction(ActionEvent actionEvent) throws IOException {
-        String url = "../views/ReportsForm.fxml";
-        navigate(url, "Reports");
-    }
 
-    public void btnAddNewPayment_OnAction(ActionEvent actionEvent) throws IOException {
-        String url = "../views/AddNewPaymentForm.fxml";
-        navigate(url, "Add New Payment");
-    }
-
-    public void btnManageUsers_OnAction(ActionEvent actionEvent) throws IOException {
-        String url = "../views/ManageUserForm.fxml";
-        navigate(url, "Manage User");
-    }
-
-    public void btnDashboard_OnAction(ActionEvent actionEvent) throws IOException {
-        String url = "../views/DashboardForm.fxml";
-        navigate(url, "DashBoard");
-    }
-
-    public void btnManageUser_OnKeyPress(KeyEvent keyEvent) throws IOException {
-        if ((keyEvent.getCode() == KeyCode.getKeyCode("Enter")) || (keyEvent.getCode() == KeyCode.getKeyCode("Space"))) {
-            String url = "../views/ManageUserForm.fxml";
-            navigate(url, "Manage Users");
-        }
-    }
-
-    public void btnAddNewPayment_OnKeyPress(KeyEvent keyEvent) throws IOException {
-        if ((keyEvent.getCode() == KeyCode.getKeyCode("Enter")) || (keyEvent.getCode() == KeyCode.getKeyCode("Space"))) {
-            String url = "../views/AddNewPaymentForm.fxml";
-            navigate(url, "Add New Payment");
-        }
-    }
-
-    public void btnReports_OnKeyPress(KeyEvent keyEvent) throws IOException {
-        if ((keyEvent.getCode() == KeyCode.getKeyCode("Enter")) || (keyEvent.getCode() == KeyCode.getKeyCode("Space"))) {
-            String url = "../views/ReportsForm.fxml";
-            navigate(url, "Reports");
-        }
-    }
-
-    public void btnDashboard_OnKeyPress(KeyEvent keyEvent) throws IOException {
-        if ((keyEvent.getCode() == KeyCode.getKeyCode("Enter")) || (keyEvent.getCode() == KeyCode.getKeyCode("Space"))) {
-            String url = "../views/DashboardForm.fxml";
-            navigate(url, "Dashboard");
-        }
-    }
-
-    public void navigate(String url, String title) throws IOException {
-        Stage stage = (Stage) contextOfMainForm.getScene().getWindow();
-        Parent root = FXMLLoader.load(this.getClass().getResource(url));
-        Scene dashboard = new Scene(root);
-        stage.setScene(dashboard);
-        lblTitle.setText(title);
-        stage.setResizable(false);
-        stage.show();
-    }
 }
